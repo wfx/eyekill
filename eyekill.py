5f1a2c3b4ee185f73d3da5f5ebddffb073efce40
Wolfgang Morawetz <wolfgang.morawetz@gmail.com>
2013-09-08 18:53:11 +0200

    rewrite: eyekill.desktop rewrite: eyekill.py

--- eyekill.py
+++ eyekill.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2013, Wolfgang Morawetz.
 # All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
-# version: 0.1
+#
+# Thank's to: 
+# kuuko,and davemds from e/irc.
+# hbytes 99.99% based on the code from http://www.guguncube.com
+#
+#
+# version: 0.2
 
 import os
 import signal
@@ -16,53 +21,119 @@ import edje
 import ecore
 import evas
 
+def destroy(obj):
+    elementary.exit()
 
-def application(userid = os.getuid()):
+# - Application - {{{
+def app(userid = os.getuid()):
+    # Main Window
+    win = elementary.Window("my app", elementary.ELM_WIN_BASIC)
+    win.title_set("eye kill")
+    win.callback_delete_request_add(destroy)
 
-    app_win = elementary.StandardWindow("Main", "Eye Kill")
-    app_win.callback_delete_request_add(lambda o: elementary.exit())
+    bg = elementary.Background(win)
+    win.resize_object_add(bg)
+    bg.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    bg.show()
+    
 
-    process_list = elementary.List(app_win)
-    app_win.resize_object_add(process_list)
-    process_list.size_hint_weight_set(evas.EVAS_HINT_EXPAND,evas.EVAS_HINT_EXPAND)
-    update_process_list(process_list, userid)
-    process_list.show()
+    # View: Information(PID)
+    #       Box
+    main_box = elementary.Box(win)
+    main_box.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    win.resize_object_add(main_box)
+    main_box.show()
+    #       Box -> Frame
+    info_frame = elementary.Frame(win)
+    info_frame.text_set("Information")
+    main_box.pack_end(info_frame)
+    info_frame.show()
+    #       Box -> Frame -> Label (default Information)
+    lb = elementary.Label(win)
+    lb.text_set('<b>Wow</b>')
+    info_frame.content_set(lb)
+    lb.show()
 
-    app_win.resize(420,426)
-    app_win.show()
 
-
-def update_process_list(process_list, userid):
-    pid_list = psutil.get_pid_list()
-    for i in pid_list:
+    # View: PID List
+    #       Box -> GenList
+    pid_list = elementary.Genlist(win)
+    pid_list.callback_clicked_double_add(kill_bill)
+    win.resize_object_add(pid_list)
+    pid_list.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+    pid_list.size_hint_align_set(evas.EVAS_HINT_FILL, evas.EVAS_HINT_FILL)
+    pid_list_itc = elementary.GenlistItemClass(
+                        item_style="default",
+                        text_get_func=pid_list_text_get,
+                        content_get_func=pid_list_content_get,
+                        state_get_func=pid_list_state_get)
+    
+    plist = psutil.get_pid_list()
+    for i in plist:
         pid = psutil.Process(i)
         if (pid.uids.real == userid):
-            process_list.item_append(pid.name, callback = process_list_menu, cb_data = pid)
+            pid_list.item_append(pid_list_itc, pid.pid, func=update_main_box)
 
+    main_box.pack_end(pid_list)
+    pid_list.show()
 
-def process_list_menu(li, it, cb_data):
-    popup = elementary.Ctxpopup(li)
-    #popup.item_append("Info", func = process_info, cb_data = cb_data)
-    popup.item_append("Kill", func = process_kill, cb_data = cb_data)
-    (x, y) = li.evas.pointer_canvas_xy_get()
-    popup.move(x, y)
-    popup.show()
+    
+    # View: Application
+    win.resize(432, 400)
+    win.show()
+#}}}
 
+# - Label Process information - {{{
+def update_main_box(gli, gl, *args, **kwargs):
+    bill = psutil.Process(gli.data_get())
+    print ("\n--- Process Information ---")
+    print ("PID   : %s" % bill.pid)
+    print ("Name  : %s" % bill.name)
+    print ("Mem   : %s" % hbytes(bill.get_memory_info().vms))
+    print ("CPU/P : %s" % bill.get_cpu_percent())
+#}}}
 
-def process_info(li, it, cb_data):
-    pass
+# - Kill Bill :-) - {{{
+def kill_bill(gli, gl):
+    bill = gl.data_get()
+    print ("Bill %s ... Gotcha" % bill)
+    os.kill(bill, signal.SIGTERM)
+    if (os.kill(bill, 0)):
+        os.kill(bill, signal.SIGKILL)
+    gli.delete()
+#}}}
 
+# - Pid List Text Get - {{{
+def pid_list_text_get(obj, part, item_data):
+    p = psutil.Process(item_data)
+    return "%s :: %s" % (p.pid, p.name)
+#}}}
 
-def process_kill(li, it, cb_data):
-    os.kill(cb_data.pid, signal.SIGTERM)
-    if (os.kill(cb_data.pid, 0)):
-        os.kill(cb_data.pid, signal.SIGKILL)
+# - Pid List Content Get - {{{
+def pid_list_content_get(obj, part, data):
+    ic = elementary.Icon(obj)
+    ic.file_set("images/logo_small.png")
+    ic.size_hint_aspect_set(evas.EVAS_ASPECT_CONTROL_VERTICAL, 1, 1)
+    return ic
+#}}}
 
+# - Pid List State Get - {{{
+def pid_list_state_get(obj, part, item_data):
+    return False
+#}}}
+
+# - Human Bytes from Guguncube - {{{
+def hbytes(num):
+    for x in ['bytes','KB','MB','GB']:
+        if num < 1024.0:
+            return "%3.2f%s" % (num, x)
+        num /= 1024.0
+    return "%3.2f%s" % (num, 'TB')
+#}}}
 if __name__ == "__main__":
-
     elementary.init()
-    application()
-
+    app()
     elementary.run()
     elementary.shutdown()
-    
+
+# vim:foldmethod=marker 
