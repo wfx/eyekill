84dc48d33f56df51a33e847d9e5547e6943d8f1f
Wolfgang Morawetz <wolfgang.morawetz@gmail.com>
2013-09-10 21:53:27 +0200

    Code cleanup. Intialy process filter avail.

--- eyekill.py
+++ eyekill.py
@@ -13,9 +13,10 @@
 #
 # version: 0.9 (bug hunt)
 #
-# TODO: Testing
-#       Code cleanup
-#       Paint a application icon
+# TODO: - Testing
+#       o Code cleanup
+#       x Filter for all or Elementary processes
+#       x Paint a application icon
 #
 #
 
@@ -28,94 +29,110 @@ import edje
 import ecore
 import evas
 
-def destroy(obj):
-    elementary.exit()
 
-# - Application - {{{
-def app(userid = os.getuid()):
-    # Main Window
-    global win, main_box
-    win = elementary.Window("my app", elementary.ELM_WIN_BASIC)
-    win.title_set("eye kill")
-    win.callback_delete_request_add(destroy)
+class Application(object):
 
-    bg = elementary.Background(win)
-    win.resize_object_add(bg)
-    bg.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
-    bg.show()
 
-    # View: Information(PID)
-    #       Box
-    main_box = elementary.Box(win)
-    main_box.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
-    win.resize_object_add(main_box)
-    main_box.show()
-    #       Box -> Frame
-    info_frame = elementary.Frame(win)
-    info_frame.text_set("Information")
-    main_box.pack_end(info_frame)
-    info_frame.show()
-    #       Box -> Frame -> Label (default Information)
-    lb = elementary.Label(win)
-    lb.text_set('<b>Kill process with a double click</b>')
-    info_frame.content_set(lb)
-    lb.show()
+    def __init__(self, parent_pid = None):
+        self.userid = os.getuid()
+        self.parent_pid = parent_pid
 
-    # View: PID List
-    #       Box -> GenList
-    ps_list = elementary.List(win)
-    ps_list.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
-    ps_list.size_hint_align_set(evas.EVAS_HINT_FILL, evas.EVAS_HINT_FILL)
-    ps_list.callback_clicked_double_add(kill_bill)
-    pid_list = psutil.get_pid_list()
-    for i in pid_list:
-        ps = psutil.Process(i)
-        if (ps.uids.real == userid):
-            short_info = '%s / %s / %s' % (ps.pid, ps.name, ps.status)
-            ps_list.item_append(label = short_info, callback = update_info, ps=ps, lb=lb)
+        self.win = elementary.Window("my app", elementary.ELM_WIN_BASIC)
+        self.win.title_set("eye kill")
+        self.win.callback_delete_request_add(self.destroy)
 
-    main_box.pack_end(ps_list)
-    ps_list.go()
-    ps_list.show()
+        self.bg = elementary.Background(self.win)
+        self.win.resize_object_add(self.bg)
+        self.bg.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+        self.bg.show()
+
+        self.main_box = elementary.Box(self.win)
+        self.main_box.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+        self.win.resize_object_add(self.main_box)
+        self.main_box.show()
+        self.info_frame = elementary.Frame(self.win)
+        self.info_frame.text_set("Information")
+        self.main_box.pack_end(self.info_frame)
+        self.info_frame.show()
+        self.lb = elementary.Label(self.win)
+        self.lb.text_set('<b>Kill process with a double click</b>')
+        self.info_frame.content_set(self.lb)
+        self.lb.show()
+
+        self.ps_list = elementary.List(self.win)
+        self.ps_list.size_hint_weight_set(evas.EVAS_HINT_EXPAND, evas.EVAS_HINT_EXPAND)
+        self.ps_list.size_hint_align_set(evas.EVAS_HINT_FILL, evas.EVAS_HINT_FILL)
+        self.ps_list.callback_clicked_double_add(self.kill_bill)
+
+        self.update_list()
+
+        self.main_box.pack_end(self.ps_list)
+        self.ps_list.go()
+        self.ps_list.show()
     
-    # View: Application
-    win.resize(320, 384)
-    win.show()
-#}}}
-
-# - Label Process information - {{{
-def update_info(li, it, lb, ps):
-    info = \
-        ("<b>PID: <i>%i</i></b>, <b>Name :<i>%s</i></b></br><b>Memory (vms) :<i>%s</i><b>, <b>CPU: <i>%s</i><b>, <b>State: <i>%s</i><b>" % \
-        (ps.pid, ps.name, hbytes(ps.get_memory_info().vms), ps.get_cpu_percent(interval=.1), ps.status ))
-    lb.text_set(info)
-#}}}
-
-# - Kill Bill :-) - {{{
-def kill_bill(obj, cb_data):
-    bill = cb_data.data_get()[1]['ps'].pid
-    print ("Bill %s ... Gotcha" % bill)
-    os.kill(bill, signal.SIGTERM)
-    if (os.kill(bill, 0)):
-        os.kill(bill, signal.SIGKILL)
-    item = obj.selected_item_get()
-    item.disabled_set(True)
-#}}}
+        self.win.resize(320, 384)
+        self.win.show()
 
 
-# - Human Bytes from Guguncube - {{{
+    def destroy(self, obj):
+        elementary.exit()
+
+
+    def update_list(self):
+        if self.parent_pid is None:
+            pl = psutil.get_pid_list()
+            for p in pl:
+                p = psutil.Process(p)
+                if p.uids.real == self.userid:
+                    short_info = '%s / %s / %s' % (p.pid, p.name, p.status)
+                    self.ps_list.item_append(label = short_info, callback = self.update_info, p = p)
+        else:
+            ps = psutil.Process(self.parent_pid)
+            pl = ps.get_children(self.parent_pid)
+            for p in pl:
+                if p.uids.real == self.userid:
+                    short_info = '%s / %s / %s' % (p.pid, p.name, p.status)
+                    self.ps_list.item_append(label = short_info, callback = self.update_info, p = p)
+
+
+    def update_info(self,li , it, p):
+        info = ("<b>PID: <i>%i</i></b>,\
+                <b>Name :<i>%s</i></b>\
+                </br><b>Memory (vms) :<i>%s</i><b>, \
+                <b>CPU: <i>%s</i><b>, \
+                <b>State: <i>%s</i><b>" % \
+                (p.pid, p.name,hbytes(p.get_memory_info().vms), p.get_cpu_percent(interval=.1), p.status ))
+        self.lb.text_set(info)
+
+
+    def kill_bill(self, obj, cb_data):
+        bill = cb_data.data_get()[1]['p'].pid
+        print ("Bill %s ... Gotcha" % bill)
+        os.kill(bill, signal.SIGTERM)
+        if (os.kill(bill, 0)):
+            os.kill(bill, signal.SIGKILL)
+        item = obj.selected_item_get()
+        item.disabled_set(True)
+
+
 def hbytes(num):
     for x in ['bytes','KB','MB','GB']:
         if num < 1024.0:
             return "%3.2f%s" % (num, x)
         num /= 1024.0
     return "%3.2f%s" % (num, 'TB')
-#}}}
+
+def get_de():
+    de = None
+    #Hmmm ......
+    return de
 
 
 if __name__ == "__main__":
     elementary.init()
-    app()
+
+    Application(get_de())
+    
     elementary.run()
     elementary.shutdown()
 
